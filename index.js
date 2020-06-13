ae900f68552e6c943f99b898d4a436f5b2e0a634
Samantha Lurio <samantha.lurio@gmail.com>
2020-06-13 01:24:39 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function updateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var newObject = Object.assign({}, object, {[key]: value});
+  return newObject;
+}
